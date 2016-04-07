3a2c19c9484acf1211d408492d8d47f4df16f287
TRa <kurogelabs@gmail.com>
2016-04-07 06:34:07 +0000

    testing continues

--- test2.php
+++ test2.php
@@ -10,6 +10,16 @@ $nameType="1";
 $surName="Smith";
 $firstName="John";
 $middleName="Edward";
+$addressCount="1";
+$addressType="1"; //permanent
+$addressStatus="n"; //normal
+$addressBegin="2002.01.18";
+$addressEnd="2100.01.01";
+$streetAddress="Main street 11";
+$city="Springfield";
+$zipCode="90210";
+$country="United States";
+$phone="040-12314555";
 //$input = "2700012915";
 print str_pad($patronID, 10, " ", STR_PAD_RIGHT);
 print str_pad($barcodeID, 10, " ", STR_PAD_RIGHT);
@@ -26,6 +36,21 @@ print str_pad($surName, 20, " ", STR_PAD_RIGHT);
 print str_pad($firstName, 20, " ", STR_PAD_RIGHT);
 print str_pad($middleName, 20, " ",STR_PAD_RIGHT);
 print str_pad($barcode, 75, " ");
+print str_pad($addressCount, 1, " ");
+print str_pad($barcode, 10, " ");
+print str_pad($addressType, 1, " ");
+print str_pad($addressStatus, 1, " ");
+print str_pad($addressBegin,10," ");
+print str_pad($addressEnd,10," ");
+print str_pad($streetAddress,50," ",STR_PAD_RIGHT);
+print str_pad($barcode,160," ");
+print str_pad($city, 40, " ",STR_PAD_RIGHT);
+print str_pad($barcode,7, " ");
+print str_pad($zipCode,10, " ",STR_PAD_RIGHT);
+print str_pad($country,20, " ",STR_PAD_RIGHT);
+print str_pad($phone,25," ",STR_PAD_RIGHT);
+print str_pad($barcode,85," ");
+print "\n";
 
 // print str_pad($input, 40, "0", STR_PAD_LEFT); 
 ?>
